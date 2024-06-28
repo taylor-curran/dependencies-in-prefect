d6f45f6639c79e53c3ddcd8778c7eed993d46a3b
Taylor Curran <taylorcurranagtc@gmail.com>
2024-06-28 15:09:36 -0500

    adding alternative example

--- README.md
+++ README.md
@@ -80,8 +80,15 @@ def flow_of_sequential_tasks():
     fut = None
     for num in nums:
         fut = task1.submit(num, wait_for=[fut] if fut is not None else [])
+```
 
+OR if there's a preexisting task's future you're waiting for, you don't need to account for the None case
 
-if __name__ == "__main__":
-    flow_of_sequential_tasks()
+```python
+@flow
+def flow_of_sequential_tasks():
+    fut = task1.submit()
+    nums = [1, 2, 3]
+    for num in nums:
+        fut = task1.submit(num, wait_for=[fut])
 ```
--- example_scripts/dynamic_tasks.py
+++ example_scripts/dynamic_tasks.py
@@ -15,5 +15,25 @@ def flow_of_sequential_tasks():
         fut = task1.submit(num, wait_for=[fut] if fut is not None else [])
 
 
+# --------------------------------------------------------------------------------
+
+## OR if there's a preexisting task's future you're waiting for, you don't need to account for the None case
+
+from prefect import flow, task
+
+
+@task
+def task1(num):
+    print(f"Task {num}")
+
+
+@flow
+def flow_of_sequential_tasks():
+    fut = task1.submit()
+    nums = [1, 2, 3]
+    for num in nums:
+        fut = task1.submit(num, wait_for=[fut])
+
+
 if __name__ == "__main__":
     flow_of_sequential_tasks()
